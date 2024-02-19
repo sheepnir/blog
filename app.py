d62030a218b622834b8c1167712022cd53dbde18
nirs <sheep.nir@gmail.com>
2024-02-18 16:30:15 -0800

    init 2

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
+import os
 from flask import Flask
+
 app = Flask(__name__)
 
-# Setting Hello from Flask file
 @app.route('/')
-def hello():
-    return 'Hello from Flask!'
+def hello_world():
+    return 'Hi from Flask!'
+
+if __name__ == '__main__':
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0', port=port)
